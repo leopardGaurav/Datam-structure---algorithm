3fbe44aed14f01a93f33855bc3c605562b43fa0e
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-06 17:10:23 +0530

    Create largest.java

---
+++ conditional statement/largest.java
@@ -0,0 +1,15 @@
+public class largest {
+    public static void main(String[] args) {
+        int num1 = 10;
+    int num2 = 20;
+    int num3 = 15;
+    if(num1>=num2 && num1>=num3){
+        System.out.println("num1 is largest:"+num1);
+    }else if(num2>=num1 && num2>=num3){
+        System.out.println("num2 is grater : "+num2);
+    } else{
+        System.out.println("num3 is grater : "+num3);
+    }
+    }
+
+}
