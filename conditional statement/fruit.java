ede8cf9efec053d29771da6d50aaec861c27ab15
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-07 12:39:09 +0530

    Create fruit.java

---
+++ conditional statement/fruit.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+
+public class fruit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String fruit = sc.nextLine();
+
+        // switch (fruit) {
+        //     case "mango":
+        //         System.out.println("King of fruits");
+        //         break;
+        //     case "apple":
+        //         System.out.println("A sweet red fruit");
+        //         break;
+        //     case "orange":
+        //         System.out.println("Round fruit");
+        //         break;
+        //     case "grape":
+        //         System.out.println("Small fruit");
+        //         break;
+        //     default:
+        //         System.out.println("Please enter a valid fruit name");
+        // }
+        // =========================================================
+        // switch(fruit){
+        //     case "mango" -> System.out.println("King of fruits");
+        //     case "apple" -> System.out.println("A sweet red fruit");
+        //     case "orange" -> System.out.println("Round fruit");
+        //     case "grape" -> System.out.println("Small fruit");
+        //     default -> System.out.println("Please enter a valid fruit name");
+        // }
+        // switch(day){
+        //     case 1:
+        //     case 2:
+        //     case 3:
+        //     case 4:
+        //     case 5:
+        //         System.out.println("Weekday");
+        //         break;
+        //     case 6:
+        //     case 7:
+        //         System.out.println("Weekend");
+        //         break;
+        // }
+
+        // switch(day){
+        //     case 1,2,3,4,5 -> System.out.println("Weekday");
+        //     case 6,7 -> System.out.println("Weekend");
+        // }
+        //===========================================
+        
+    }
+}
