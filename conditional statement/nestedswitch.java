86965ec4d49b2ff03ad07b880f6c743b8d90d37a
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-07 12:39:16 +0530

    Create nestedswitch.java

---
+++ conditional statement/nestedswitch.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class nestedswitch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int empId = sc.nextInt();
+        String department = sc.next();
+
+        
+    }
+}
