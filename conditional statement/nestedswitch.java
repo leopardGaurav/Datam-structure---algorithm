0a8f14108f2be042bbcb5b85331a2a172bb449d1
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-08 18:26:32 +0530

    Update nestedswitch.java

--- conditional statement/nestedswitch.java
+++ conditional statement/nestedswitch.java
@@ -6,6 +6,26 @@ public class nestedswitch {
         int empId = sc.nextInt();
         String department = sc.next();
 
-        
+        switch(empid){
+            case 1:
+                System.out.println("gaurav kumar");
+                break;
+            case 2:
+            System.out.println("krishna");
+                break;
+            case 3:
+                switch(department){
+                    case "IT":
+                        System.out.println("IT department");
+                        break;
+                    case "CSE":
+                        System.out.println("CSE department");
+                        break;
+                    default:
+                        System.out.println("No department found");
+                }
+            default:
+                System.out.println("Enter valid employee id");
+        }
     }
 }
