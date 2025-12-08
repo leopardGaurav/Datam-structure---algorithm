bbf660eecbe159d14fd2569b5280936a252ad9c9
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-08 18:26:20 +0530

    Create hellow.java

---
+++ function/hellow.java
@@ -0,0 +1,8 @@
+public class hellow {
+    public static void main(String[] args) {
+            greeting();
+    }
+    static void greeting(){
+        System.out.print("jai radha rani");
+    }
+}
