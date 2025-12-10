a7fcfe8e6e05bdebd310cefc66b7e8e7be24dec7
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-10 15:18:12 +0530

    Create overloading.java

---
+++ function/overloading.java
@@ -0,0 +1,13 @@
+public class overloading {
+    public static void main(String[] args) {
+        fun(67);
+        fun("Gaurav");
+    }
+    static void fun(int a){
+        System.out.println(a);
+    }
+
+    static void fun(String name){
+        System.out.println(name);
+    }
+}
