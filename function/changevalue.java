b86e6d3bbc3a8efdf57af4dca00377006c927bce
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-10 15:18:57 +0530

    Update changevalue.java

--- function/changevalue.java
+++ function/changevalue.java
@@ -2,7 +2,11 @@ public class changevalue{
     public static void main(String[] args) {
         //create an array
         int[] arr = {1,3,2,45,6};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void change(int[] nums){
+        nums[0] = 99;
 
     }
-    
 }
