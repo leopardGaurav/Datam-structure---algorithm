e1dc255bfafa0d11c21d0fb7ea5bdd0c1c5e4cfc
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-15 14:34:24 +0530

    Create largestelement.java

---
+++ ArrayQ/largestelement.java
@@ -0,0 +1,15 @@
+
+public class largestelement {
+    public static void main(String[] args) {
+        int[] arr = {23, 45, 67, 89, 12, 90};
+
+        int largest = arr[0];
+
+        for (int i=1;i<arr.length;i++){
+            if(arr[i]>largest){
+                largest = arr[i];
+            }
+        }
+        System.out.println("Largest element is : " + largest);
+    }
+}
