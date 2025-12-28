5bd010fe6b31432ea250ad7f6715b10b5d5bb4b4
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-29 00:18:19 +0530

    Create floor.java

---
+++ binarysearch/floor.java
@@ -0,0 +1,28 @@
+public class floor {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10,12,14,16,18,20};
+        int target = 14;
+        int result = floor(arr, target);
+         if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+    static int floor(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if (target<arr[mid]){
+                end = mid - 1;
+            } else if(target > arr[mid]){
+                start = mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return end;
+    }
+}
