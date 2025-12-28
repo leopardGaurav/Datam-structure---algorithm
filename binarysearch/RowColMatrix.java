2c162d4a70da53d5a7455c12ac0e5430f9309267
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-29 01:54:12 +0530

    Create RowColMatrix.java

---
+++ binarysearch/RowColMatrix.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+public class RowColMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {10,20,30,40},
+                {15,25,35,45},
+                {27,29,37,48},
+                {32,33,39,50}
+        };
+        System.out.println(Arrays.toString(search(matrix,29)));
+    }
+
+    static int[] search(int[][] matrix, int target){
+        int r=0;
+        int c = matrix[0].length-1;
+
+        while(r<matrix.length && c>=0){
+            if(matrix[r][c]==target){
+                return new int[]{r,c};
+            }
+            else if(matrix[r][c]>target){
+                c--;
+            }
+            else{
+                r++;
+            }
+        }
+        return new int[]{-1,-1};
+
+    }
+}
