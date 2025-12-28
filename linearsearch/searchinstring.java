bedf02aabb61b9b12afcf6da0909484eab9ed9e5
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-28 15:57:06 +0530

    Create searchinstring.java

---
+++ linearsearch/searchinstring.java
@@ -0,0 +1,19 @@
+package linearsearch;
+public class searchinstring {
+    public static void main(String[] args) {
+        String name = "gaurav";
+        char target = 'v';
+        System.out.println(search(name,target));
+    }
+    static boolean search(String str , char target){
+        if(str.length() == 0){
+            return false;
+        }
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == target){
+                return true;
+            }
+        }
+        return false;
+    }
+}
