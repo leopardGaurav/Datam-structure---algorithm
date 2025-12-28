2a6a75e8a2e549b80b1c99fb9c0a331f56a873a2
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-28 15:56:47 +0530

    Create threeSumClosest.java

---
+++ Leetcodeproblem.java/threeSumClosest.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class threeSumClosest {
+
+    public static int threeSumClosest(int[] nums, int target) {
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n - 2; i++) {
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                    closestSum = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return sum; // exact match
+                }
+            }
+        }
+        return closestSum;
+    }
+
+    // 👇 MAIN METHOD (VS Code ke liye zaroori)
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        int result = threeSumClosest(nums, target);
+        System.out.println("Closest Sum = " + result);
+
+        sc.close();
+    }
+}
